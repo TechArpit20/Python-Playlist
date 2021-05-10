4fba5c7fe3998e4ae17ce262d7ef1ec55396f807
Arpit Dadhich <65245622+TechArpit20@users.noreply.github.com>
2021-05-10 12:22:25 -0400

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,16 @@
+'''if (any boolean value):
+    print (' TRue')
+else:
+    print('False')'''
+
+a=4
+b=3
+c=5
+
+if a<b:
+    print('a is less than b')
+else:
+    print('b is greater than a')
+
+if c<5:
+    print('c is less than 5')
